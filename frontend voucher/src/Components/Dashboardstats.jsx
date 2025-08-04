f4e21927ec481c5f4405e8886f5232776821a8e1
Bassey Edubio <basseyedubio@gmail.com>
2025-08-04 11:10:49 +0100

    update

--- frontend voucher/src/Components/Dashboardstats.jsx
+++ frontend voucher/src/Components/Dashboardstats.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import Displaynumbers from './Displaynumbers'
 
-// Dashboardstats.jsx
-import React from 'react';
-import DisplayNumbers from './Displaynumbers'; 
-
 const Dashboardstats = () => {
   return (
     <div className="p-6">
