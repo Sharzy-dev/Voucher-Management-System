bf3231e9f1a0b94dba03bbda72865d2d2f078b68
uhuoifeanyi1 <ifeanyiuhuo2@gmail.com>
2025-08-05 17:40:51 +0100

    latest update on uploadhere component

--- frontend voucher/src/App.jsx
+++ frontend voucher/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Login from './Pages/Login'
-
+import Uploadhere from './Components/Uploadhere'
+import Uploadherebox from './Components/Uploadherebox'
 function App() {
   return (
-    <div>
-      <Login/>
+    <div >
+       <Login/>
+       {/* <Uploadhere/>  */}
+      {/* <Uploadherebox/> */}
     </div>
   )
 }
 
-export default App
+export default App 
