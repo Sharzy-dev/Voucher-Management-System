48777f8d6a32dbe5d041b25ec7292a4e8dfae929
uhuoifeanyi1 <ifeanyiuhuo2@gmail.com>
2025-08-05 17:40:51 +0100

    latest update on uploadhere component

--- frontend voucher/src/Components/Uploadhere.jsx
+++ frontend voucher/src/Components/Uploadhere.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 function Uploadhere() {
   return (
-    <div>
-        upload here section
-    </div>
+     <div className=' flex flex-col bg-amber-950'>
+
+    <div className=' flex flex-col w-136.5 h-134.5 ml-69 mt-26.75  gap-11  p-10  bg-white  rounded-lg  '>
+<h1 className='w-95.75 h-6 font-sans font-semibold text-2xl text-[rgba(26,26,26,1)]'>upload here </h1>
+<p className='w-95.75 h-5.5 font-sans font-[400px] text-[16px] '>File supported: pdf, jpg, and png.</p>
+
+
+
+</div>
+</div>
   )
 }
 
